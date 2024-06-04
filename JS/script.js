91597ab0b35d8b50eaa92c22658144d6ba13761f
CristianN01 <cristiannechifor71@gmail.com>
2024-06-04 11:43:57 +0200

    aggiustato messagi nei loro punti stabiliti

--- JS/script.js
+++ JS/script.js
@@ -170,7 +170,6 @@ const { createApp } = Vue
             name: 'Cristian',
             avatar: './img/avatar_io.jpg'
         },
-        currentContactProfile: 0,
         currentChat: 0,
       }
     },
